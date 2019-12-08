676908041215476f7790ee2981774e522a296a91
Fabio Papa <fabtheman@gmail.com>
2019-12-07 20:23:45 -0800

    Add functions to replace React's createElement, use it for JSX

--- src/create_element.js
+++ src/create_element.js
@@ -1,3 +1,30 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+const Didact = {
+  createElement
+};
+
+/** @jsx Didact.createElement */
 const element = (
   <div id="foo">
     <a>bar</a>
