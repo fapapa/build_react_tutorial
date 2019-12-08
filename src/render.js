7415a39989994b0705f3d8d891b5d69ce1b9ddef
Fabio Papa <fabtheman@gmail.com>
2019-12-07 20:54:36 -0800

    Replace React's render with a custom render

--- src/render.js
+++ src/render.js
@@ -20,8 +20,27 @@ function createTextElement(text) {
   };
 }
 
+function render(element, container) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = key => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach(child => render(child, dom));
+
+  container.appendChild(dom);
+}
+
 const Didact = {
-  createElement
+  createElement,
+  render
 };
 
 /** @jsx Didact.createElement */
@@ -32,4 +51,4 @@ const element = (
   </div>
 );
 const container = document.getElementById("root");
-ReactDOM.render(element, container);
+Didact.render(element, container);
